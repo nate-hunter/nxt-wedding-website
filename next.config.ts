4bb6e486cd06e6f490eb59d915837a0556c8e2a0
nate-hunter <hunter.j.nate@gmail.com>
2025-05-31 01:09:08 -1000

    updated CSS config to resolve vercel build errors.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost'],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
 };
 
 export default nextConfig;
